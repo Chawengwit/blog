a05a2e91c705d7f685d6a1bf132a1fae5b786fca
Chawengwit <cw-justice_rs106@hotmail.com>
2021-07-19 16:49:16 +0700

    layout

--- Backup/nav.js
+++ Backup/nav.js
@@ -1,6 +1,8 @@
-let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-nav-toggle');
+let mainNav = document.getElementById('js-menu');
+let logo = document.getElementById('logo-id');
 
 navBarToggle.addEventListener("click",function() {
     mainNav.classList.toggle('active');
+    logo.classList.toggle('noneActive');
 });
